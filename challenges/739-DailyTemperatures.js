ad8dec79aaa2dfd5051e0612875010f23fd94d49
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-24 10:45:31 +0000

    Add solution for 739 Daily Temperatures

---
+++ challenges/739-DailyTemperatures.js
@@ -0,0 +1,15 @@
+var dailyTemperatures = function(temperatures) {
+    let n = temperatures.length;
+    let res = Array(n).fill(0);
+    let stack = [];
+    
+    for (let i = 0; i < n; i++) {
+        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            let idx = stack.pop();
+            res[idx] = i - idx;
+        }
+        stack.push(i);
+    }
+    
+    return res;
+};
