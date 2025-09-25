2b43b3e79ffa333702161a82f272f311cc4e4432
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 18:50:35 +0100

    Create 16799-MaxNumberOfKSumPairs.js

---
+++ challenges/16799-MaxNumberOfKSumPairs.js
@@ -0,0 +1,13 @@
+var maxOperations = function(nums, k) {
+    let map = new Map(), count = 0;
+    for (let num of nums) {
+        let complement = k - num;
+        if (map.get(complement) > 0) {
+            count++;
+            map.set(complement, map.get(complement) - 1);
+        } else {
+            map.set(num, (map.get(num) || 0) + 1);
+        }
+    }
+    return count;
+};
