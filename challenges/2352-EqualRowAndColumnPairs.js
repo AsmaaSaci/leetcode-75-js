1c23cbad3cbb98dbd54c464383fa192b4ef56996
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 19:17:34 +0100

    Create 2352-EqualRowAndColumnPairs.js

---
+++ challenges/2352-EqualRowAndColumnPairs.js
@@ -0,0 +1,16 @@
+var equalPairs = function(grid) {
+    let n = grid.length, count = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let match = true;
+            for (let k = 0; k < n; k++) {
+                if (grid[i][k] !== grid[k][j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) count++;
+        }
+    }
+    return count;
+};
