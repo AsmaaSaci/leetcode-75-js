82bf71680ecf9fd4b375da01fa77d389b1d7980e
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 19:35:12 +0100

    Update 2462-TotalCostToHireKWorkers.js

--- challenges/2462-TotalCostToHireKWorkers.js
+++ challenges/2462-TotalCostToHireKWorkers.js
@@ -1,24 +1,50 @@
 var totalCost = function(costs, k, candidates) {
-    let n = costs.length;
-    let left = 0, right = n - 1;
-    let leftHeap = [], rightHeap = [];
-    let res = 0;
-    while (k--) {
-        while (leftHeap.length < candidates && left <= right) {
-            leftHeap.push(costs[left++]);
-        }
-        while (rightHeap.length < candidates && left <= right) {
-            rightHeap.push(costs[right--]);
-        }
-        let minLeft = leftHeap.length ? Math.min(...leftHeap) : Infinity;
-        let minRight = rightHeap.length ? Math.min(...rightHeap) : Infinity;
-        if (minLeft <= minRight) {
-            res += minLeft;
-            leftHeap.splice(leftHeap.indexOf(minLeft), 1);
+    const MinHeap = function() {
+        this.data = [];
+        this.size = () => this.data.length;
+        this.push = (val) => {
+            this.data.push(val);
+            let i = this.data.length - 1;
+            while (i > 0) {
+                let p = Math.floor((i-1)/2);
+                if (this.data[p] <= this.data[i]) break;
+                [this.data[p], this.data[i]] = [this.data[i], this.data[p]];
+                i = p;
+            }
+        };
+        this.pop = () => {
+            if (this.data.length === 1) return this.data.pop();
+            let top = this.data[0];
+            this.data[0] = this.data.pop();
+            let i = 0;
+            while (true) {
+                let left = 2*i+1, right = 2*i+2, smallest = i;
+                if (left < this.data.length && this.data[left] < this.data[smallest]) smallest = left;
+                if (right < this.data.length && this.data[right] < this.data[smallest]) smallest = right;
+                if (smallest === i) break;
+                [this.data[i], this.data[smallest]] = [this.data[smallest], this.data[i]];
+                i = smallest;
+            }
+            return top;
+        };
+        this.peek = () => this.data[0];
+    };
+
+    let left = 0, right = costs.length - 1;
+    let total = 0;
+    let leftHeap = new MinHeap(), rightHeap = new MinHeap();
+
+    for (let i = 0; i < candidates && left <= right; i++) leftHeap.push(costs[left++]);
+    for (let i = 0; i < candidates && left <= right; i++) rightHeap.push(costs[right--]);
+
+    for (let i = 0; i < k; i++) {
+        if (!rightHeap.size() || (leftHeap.size() && leftHeap.peek() <= rightHeap.peek())) {
+            total += leftHeap.pop();
+            if (left <= right) leftHeap.push(costs[left++]);
         } else {
-            res += minRight;
-            rightHeap.splice(rightHeap.indexOf(minRight), 1);
+            total += rightHeap.pop();
+            if (left <= right) rightHeap.push(costs[right--]);
         }
     }
-    return res;
+    return total;
 };
