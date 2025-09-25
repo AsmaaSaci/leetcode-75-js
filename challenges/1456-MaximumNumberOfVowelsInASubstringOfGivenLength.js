869c12a7c96a963c59e125dc21d00a2a880621f4
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 19:20:48 +0100

    Create 1456-MaximumNumberOfVowelsInASubstringOfGivenLength.js

---
+++ challenges/1456-MaximumNumberOfVowelsInASubstringOfGivenLength.js
@@ -0,0 +1,10 @@
+var maxVowels = function(s, k) {
+    let vowels = new Set(['a','e','i','o','u']);
+    let count = 0, maxCount = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.has(s[i])) count++;
+        if (i >= k && vowels.has(s[i - k])) count--;
+        maxCount = Math.max(maxCount, count);
+    }
+    return maxCount;
+};
