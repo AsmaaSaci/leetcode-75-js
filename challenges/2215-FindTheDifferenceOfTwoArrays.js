d154b64a291f10e1d7a910c98f8e3bfcd471d52a
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 19:21:26 +0100

    Create 2215-FindTheDifferenceOfTwoArrays.js

---
+++ challenges/2215-FindTheDifferenceOfTwoArrays.js
@@ -0,0 +1,6 @@
+var findDifference = function(nums1, nums2) {
+    let set1 = new Set(nums1), set2 = new Set(nums2);
+    let diff1 = [...set1].filter(x => !set2.has(x));
+    let diff2 = [...set2].filter(x => !set1.has(x));
+    return [diff1, diff2];
+};
