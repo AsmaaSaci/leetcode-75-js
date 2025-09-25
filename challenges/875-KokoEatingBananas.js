1347039fa83db59f0757ec8634f5f6442da68211
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 14:51:21 +0100

    Create 875-KokoEatingBananas.js

---
+++ challenges/875-KokoEatingBananas.js
@@ -0,0 +1,15 @@
+var minEatingSpeed = function(piles, h) {
+  let left = 1, right = Math.max(...piles), ans = right;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    let hours = 0;
+    for (let p of piles) hours += Math.ceil(p / mid);
+    if (hours <= h) {
+      ans = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return ans;
+};
