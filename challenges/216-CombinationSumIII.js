3ad3a6ad92521a2cd114b2b59572e0799cdb53f9
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-25 14:48:22 +0100

    Create 216-CombinationSumIII.js

---
+++ challenges/216-CombinationSumIII.js
@@ -0,0 +1,17 @@
+var combinationSum3 = function(k, n) {
+  const res = [];
+  function backtrack(start, path, sum) {
+    if (path.length === k && sum === n) {
+      res.push([...path]);
+      return;
+    }
+    if (path.length >= k || sum > n) return;
+    for (let i = start; i <= 9; i++) {
+      path.push(i);
+      backtrack(i+1, path, sum+i);
+      path.pop();
+    }
+  }
+  backtrack(1, [], 0);
+  return res;
+};
